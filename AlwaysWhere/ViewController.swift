82a97f5417577c54d6c2f3fa659f88b1d3211d39
Thom Ritterfeld <thom@ritterfeld.com>
2023-12-21 15:12:27 +0100

    Added TomTomSDK with sample

--- AlwaysWhere/ViewController.swift
+++ AlwaysWhere/ViewController.swift
@@ -6,12 +6,23 @@
 //
 
 import UIKit
+import TomTomSDKMapDisplay
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let options = MapOptions(apiKey: "")
+        let mapView = MapView(mapOptions: options)
+        view.addSubview(mapView)
+
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            view.leftAnchor.constraint(equalTo: mapView.leftAnchor),
+            view.rightAnchor.constraint(equalTo: mapView.rightAnchor),
+            view.topAnchor.constraint(equalTo: mapView.topAnchor),
+            view.bottomAnchor.constraint(equalTo: mapView.bottomAnchor),
+        ])
     }
 
 
---
+++ README.md
@@ -0,0 +1,8 @@
+## Demo
+This project demonstrates how the TomTom iOS SDK requests location permissions directly on launch. Usually the developer should choose the moment to request this permission. Or for example use the map withotu the current location.
+
+### Getting started
+
+1. Don't forget to add your maps TomTom api key: ViewController.swift:15
+2. Run the project
+3. See that the location is directly requested.
